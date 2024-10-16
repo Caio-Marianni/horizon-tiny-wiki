a46c1654cfdbe9422886a5e1f5f7a383c476383b
Caio <caiomarianni@gmail.com>
2024-10-15 22:33:45 -0300

    I cant do it anymore

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -7,7 +7,7 @@ interface BackgroundProps {
   width: number;
   height: number;
   quality: number;
-  placeholder?: "blur" | "empty"; // Tipagem corrigida
+  placeholder?: "blur" | "empty";
   speed?: number;
   className?: string;
 }
@@ -23,14 +23,9 @@ const Background: React.FC<BackgroundProps> = ({
   className,
 }) => {
   const [offsetY, setOffsetY] = useState(0);
-  const [isClient, setIsClient] = useState(false); // Novo state para verificar se estamos no cliente
 
-  // Hook para garantir que a renderização ocorra apenas no cliente
   useEffect(() => {
-    // Set isClient para true após a montagem no cliente
-    setIsClient(true);
-
-    // Verifique se window está disponível
+    // Verifica se estamos no cliente (o window só existe no cliente)
     if (typeof window !== "undefined") {
       const handleScroll = () => {
         setOffsetY(window.scrollY);
@@ -45,12 +40,6 @@ const Background: React.FC<BackgroundProps> = ({
     }
   }, []);
 
-  // Se não for o cliente, não renderize o componente
-  if (!isClient) {
-    return null; // Evita renderizar no lado do servidor
-  }
-
-  // Verifique se o `src` é do tipo `StaticImageData` e obtenha a URL correta
   const blurDataUrl = typeof src === "object" ? src.src : src;
 
   return (
@@ -61,8 +50,8 @@ const Background: React.FC<BackgroundProps> = ({
       width={width}
       height={height}
       quality={quality}
-      placeholder={placeholder} 
-      blurDataURL={blurDataUrl} 
+      placeholder={placeholder}
+      blurDataURL={blurDataUrl}
       className={`${className} pointer-events-none`}
       style={{
         transform: `translateY(${offsetY * (speed / 10)}px)`, // Efeito parallax baseado na rolagem

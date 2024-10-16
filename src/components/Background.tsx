86ff8671dbf6e627bb50b5fac1522a8d26d05a54
Caio <caiomarianni@gmail.com>
2024-10-15 22:39:16 -0300

    .-.

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,4 +1,3 @@
-'use client'
 import React, { useState, useEffect } from "react";
 import Image, { StaticImageData } from "next/image";
 
@@ -8,7 +7,7 @@ interface BackgroundProps {
   width: number;
   height: number;
   quality: number;
-  placeholder?: "blur" | "empty";
+  placeholder?: "blur" | "empty"; // Tipagem corrigida
   speed?: number;
   className?: string;
 }
@@ -25,22 +24,23 @@ const Background: React.FC<BackgroundProps> = ({
 }) => {
   const [offsetY, setOffsetY] = useState(0);
 
+  // Adiciona verificação para garantir que o código está rodando no cliente
   useEffect(() => {
-    // Verifica se estamos no cliente (o window só existe no cliente)
     if (typeof window !== "undefined") {
       const handleScroll = () => {
         setOffsetY(window.scrollY);
       };
-
+      
+      // Adiciona o evento de rolagem quando o cliente estiver disponível
       window.addEventListener("scroll", handleScroll);
-
-      // Limpeza do evento ao desmontar o componente
+      
       return () => {
         window.removeEventListener("scroll", handleScroll);
       };
     }
   }, []);
 
+  // Verifique se o `src` é do tipo `StaticImageData` e obtenha a URL correta
   const blurDataUrl = typeof src === "object" ? src.src : src;
 
   return (
@@ -51,8 +51,8 @@ const Background: React.FC<BackgroundProps> = ({
       width={width}
       height={height}
       quality={quality}
-      placeholder={placeholder}
-      blurDataURL={blurDataUrl}
+      placeholder={placeholder} 
+      blurDataURL={blurDataUrl} 
       className={`${className} pointer-events-none`}
       style={{
         transform: `translateY(${offsetY * (speed / 10)}px)`, // Efeito parallax baseado na rolagem

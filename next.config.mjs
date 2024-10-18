bb36003045c656c6e725d897fc30f62cfd7b999f
Caio <caiomarianni@gmail.com>
2024-10-18 02:36:19 -0300

    Optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-module.exports = withBundleAnalyzer({
-  // Configuração adicional...
-});
-
 export default nextConfig;

397079fc50273cab645f978b017aa84e5208255d
Caio <caiomarianni@gmail.com>
2024-10-18 02:11:14 -0300

    .-. Boquinha de veludo

--- src/core/utils/ScrollRevealComponent.tsx
+++ src/core/utils/ScrollRevealComponent.tsx
@@ -1,34 +1,21 @@
 "use client";
 import { useEffect } from "react";
+import ScrollReveal from "scrollreveal"; // Importa diretamente para garantir o tipo correto
 
 const ScrollRevealComponent = () => {
   useEffect(() => {
     if (typeof window !== "undefined") {
-      // Importa ScrollReveal dinamicamente
-      import("scrollreveal").then((scrollReveal) => {
-        // Algumas versões do ScrollReveal podem precisar de sr() em vez de sr.reveal()
-        const sr = scrollReveal.default || scrollReveal;
-        
-        // Verifique se sr é uma função ou objeto e chame apropriadamente
-        if (typeof sr === "function") {
-          sr().reveal(".reveal", {
-            distance: "50px",
-            duration: 1000,
-            easing: "ease-in-out",
-            origin: "bottom",
-            reset: true,
-            interval: 200,
-          });
-        } else if (sr && typeof sr.reveal === "function") {
-          sr.reveal(".reveal", {
-            distance: "50px",
-            duration: 1000,
-            easing: "ease-in-out",
-            origin: "bottom",
-            reset: true,
-            interval: 200,
-          });
-        }
+      // Certifique-se de usar o tipo correto
+      const sr: scrollReveal.ScrollRevealObject = ScrollReveal();
+
+      // Inicializa o ScrollReveal se estiver disponível
+      sr.reveal(".reveal", {
+        distance: "50px",
+        duration: 1000,
+        easing: "ease-in-out",
+        origin: "bottom",
+        reset: true,
+        interval: 200,
       });
     }
   }, []);
